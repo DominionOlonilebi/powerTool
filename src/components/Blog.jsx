8f8a5ec7931bee870b8909926abb80dc06a00934
unknown <dominionolufunke27@gmail.com>
2024-03-02 19:58:28 +0100

    updates

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -8,14 +8,14 @@ const Blog = () => {
         <h4 className="mb-4">Latest information from our blog</h4>
         <hr className="blog_line mb-4" />
         <Row className="d-flex flex-row justify-content-center">
-          <Col md={4} className="mb-3">
-            <img src="Images/pw5.jpg" alt="" className="img-fluid" />
+          <Col md={3} className="mb-4">
+            <img src="Images/pw16.jpg" alt="" className="img-fluid" />
           </Col>
-          <Col md={4} className="mb-3">
-            <img src="Images/pw5.jpg" alt="" className="img-fluid" />
+          <Col md={3} className="mb-4">
+            <img src="Images/pw11.jpg" alt="" className="img-fluid" />
           </Col>
-          <Col md={4} className="mb-3">
-            <img src="Images/pw5.jpg" alt="" className="img-fluid" />
+          <Col md={3} className="mb-4">
+            <img src="Images/pw16.jpg" alt="" className="img-fluid" />
           </Col>
         </Row>
       </Container>
